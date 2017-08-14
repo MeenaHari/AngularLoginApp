707a5ea6c7567b701e565f022c88c0ef871c44b2
MeenaHari <meenatsnh@gmail.com>
2017-08-13 22:39:28 -0700

    Added Config file

--- conf.js
+++ conf.js
@@ -4,8 +4,7 @@ exports.config = {
           directConnect: true,  //directly run the selenium web driver standalone server
           framework: 'jasmine2',   // uses jasmine framework
           //specs: ['./StrewProject/RegistrationPage.js'],
-          specs: ['./StrewProject/RegistrationPage.js','./StrewProject/LoginPage.js'],// to call test script 
-         
-        
+          specs: ['./StrewProject/RegistrationPage.js','./StrewProject/LoginPage.js'], // to call test script 
+                
           
 }
